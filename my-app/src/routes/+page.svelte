d5dbbe049fe88785f76851b266c07eb8d603149a
Georgina <88046437+GeorginaBerrezel@users.noreply.github.com>
2023-10-24 18:24:44 +0200

    First commit

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    let count = 0;
+
+    function incrementBtn() {
+        count += 1;
+    }
+</script>
+
+<h1>Number : {count}</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<a href="/about">Go to about</a>
+<button on:click={incrementBtn}>Increment</button>
+
+<style>
+    p {
+        font-size : 2rem;
+    }
+</style>
